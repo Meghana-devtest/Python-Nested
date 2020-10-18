e5a4cc30351008d25de1ada3fe1da3bbb7d8e964
Meghana-devtest <73066975+Meghana-devtest@users.noreply.github.com>
2020-10-18 20:40:29 +0530

    Update Python-Nested.py

--- Python-Nested.py
+++ Python-Nested.py
@@ -1,5 +1,8 @@
 #Script is to retrive the key value pairs(nested)
 
+#dataretrival is function created
+#for iterations ,for loop amd if is used.
+
 def dataretrival(Employee_details):
     for key, value in Employee_details.items():
         if type(value) is dict:
@@ -9,6 +12,7 @@ def dataretrival(Employee_details):
 
 Employee_details = {"meghana": {"employee_id": 1234},"abc": {"employee_id": 2345}}
 
+#outputs which can be retrived
 dataretrival(Employee_details)
 dataretrival(Employee_details['meghana'])
 print(dataretrival(Employee_details['meghana']))
